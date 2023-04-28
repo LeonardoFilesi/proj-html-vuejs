9b9702b3db807fa134f1ce0a50f2530de958fc35
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-04-28 15:40:44 +0200

    feat: :sparkles: Adds: jumbotron working meh, but these are details

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -62,6 +62,16 @@ export default {
         <a href="#" v-for="element in store.jumbotron_bar" :key="element.id" class="ms_jumbo-bar">{{ element.text }}</a>
         <span v-for="element in store.jumbotron_bar" :key="element.id" class="ms_jumbo-bar">{{ element.icon }}</span>
     </section>
+    <section class="ms_jumbotron">
+        <div class="ms_jumbo-left">
+            <h1>Udemy Cringe Cosplay Sales</h1>
+            <h3>Monetize your cringe ideas and disgust new customers with Udemy</h3>
+        </div>
+        <div class="ms_jumbo-right">
+
+        </div>
+        <img src="../assets/img/cringe-cosplay-jumbo.jpg" alt="">
+    </section>
   </div>
 </template>
 
@@ -112,12 +122,32 @@ input {
 .ms_jumbotron-bar{
     background-color: $ebonyclay;
     width: 100%;
-    margin: 0 auto;
+    margin: 0 auto 30px auto;
     padding: 20px 0;
     justify-content: center;
 }
 .ms_jumbo-bar {
     color: $white;
     margin: 0 5px;
+}
+.ms_jumbotron {
+    width: 100;
+    position: relative;
+}
+.ms_jumbo-left {
+    position: absolute;
+    left: 0;
+    width: 80%;
+    height: 100%;
+    background-image: linear-gradient(to right, #ff4550, rgba(255, 117, 117, 0));
+    color: $white;
+}
+.ms_jumbo-right {
+    position: relative;
+    width: 100%;
+}
+.ms_jumbo-right img {
+    position: absolute;
+    right: 0;
 }
 </style>
